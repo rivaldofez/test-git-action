2a2ea7df7bf5bd88e764a561708a9d34180280d4
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-02-09 17:06:03 +0700

    Test Unit Test False

--- TestGitActionTests/TestGitActionTests.swift
+++ TestGitActionTests/TestGitActionTests.swift
@@ -25,7 +25,7 @@ final class TestGitActionTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
         
-        XCTAssertEqual("Hello", "Hello")
+        XCTAssertEqual("Hello", "Hellos")
     }
 
     func testPerformanceExample() throws {
