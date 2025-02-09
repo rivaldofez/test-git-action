bcbe3f971b4a231383874537c8ec014390e8e3e2
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-02-09 16:17:32 +0700

    setup base project

---
+++ TestGitAction/TestGitActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitActionApp.swift
+//  TestGitAction
+//
+//  Created by Rivaldo Fernandes on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGitActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
